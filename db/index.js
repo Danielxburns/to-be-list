f90589cba91c11492e13814fe20745709bf1ce7a
Danielxburns <41316262+Danielxburns@users.noreply.github.com>
2020-01-04 16:38:17 -0600

    basic back end working

--- db/index.js
+++ db/index.js
@@ -1,33 +1,77 @@
 const mongoose = require('mongoose');
 
+/* ------- SETUP CONNECTION TO DATABASE ------- SECTION */
+
 const db = mongoose.connection;
 
-mongoose.connect('mongodb://localhost/tobe', { useNewUrlParser: true });
+mongoose
+  .connect('mongodb://localhost:27017/tobe', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .catch((err) => console.log('could not connect to the ToBe database :', err));
 
-db.on('error', console.error.bind(console, 'connection error: '));
+db.on('error', (err) => {
+  console.log('There was an error while connected to the ToBe database :', err);
+});
+
 db.once('open', () => {
   console.log(`
-You are experiencing...
-        _=_
-      q(-_-)p
-      '_) (_'
-      /__/  \\
-    _(<_   / )_
-   (__\\_\\_|_/__)
-  a deep connection
-with the ToBe database.`);
+    You are experiencing...
+            _=_
+          q(-_-)p
+          '_) (_'
+          /__/  \\
+        _(<_   / )_
+        (__\\_\\_|_/__)
+      a deep connection
+    with the ToBe database.
+    `);
 });
 
-let userSchema = mongoose.Schema({
-  id: Number,
+/* --------------- SET UP SCHEMA -------------- SECTION */
+
+const userSchema = mongoose.Schema({
   name: String,
-  avatar: String,
-  sessions: [{ id: String, date: Date, todos: Array, focus: String, forwardTo: String }]
+  /*   avatar: String, */
+  sessions: [
+    { id: String, date: Date, todos: Array, focus: String, forwardTo: String }
+  ]
 });
 
-let User = mongoose.model('user', userSchema);
+const User = mongoose.model('user', userSchema);
 
-// get users on mount to display in a drop down
+/* ------------- SET UP DB QUERIES ------------- SECTION */
 
-// I want to post name, focus and todos when submitted and increment Id 
-//just start with name
+const getUsers = (cb) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      // NOTE it would be nice to send a message to the user here
+      console.log('error getting all users from database:', err);
+    } else {
+      const userMap = {};
+      users.forEach((user) => {
+        userMap[user._id] = user;
+      });
+      cb(null, userMap);
+    }
+  });
+};
+
+const postUser = (name, cb) => {
+  const newUser = new User(name);
+  console.log('inside postUser - newUser :', newUser);
+  newUser.save((err, res) => {
+    if (err) {
+      return console.error(err);
+    } else {
+      console.log('inside save res :', res);
+      return cb(null, res);
+    }
+  });
+};
+
+module.exports = {
+  getUsers,
+  postUser
+};
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const port = 3000;
 const app = express();
+const db = require('../db/index.js');
+
 app.use(cors());
 app.use(express.json());
 
@@ -9,4 +11,25 @@ app.use('/', express.static(__dirname + '/../client/public'));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+app.get('/users', (req, res) => {
+  console.log('inside GET req.body :', JSON.stringify(req.body));
+  db.getUsers((err, users) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('inside GET users :', users);
+    res.send(users);
+  });
+});
+
+app.post('/users', (req, res) => {
+  console.log('inside POST req.body :', JSON.stringify(req.body));
+  db.postUser(req.body, (err, res) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('POSTed to database user :', res.body);
+  });
 });
